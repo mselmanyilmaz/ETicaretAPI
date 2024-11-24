f403f2937399340b1397267f5d80747453d4e3b8
msyilmaz <mselman.yilmaz@spk.gov.tr>
2024-11-25 00:02:52 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ETicaretAPI.Persistance.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ETicaretAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersitanceServices(this IServiceCollection services)
+        {
+            
+
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+        }
+    }
+}
